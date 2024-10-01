5e2ba17360c18713b9fc5b2dfb0cd8cbc7f57a02
mono789 <juan.rivera7@udea.edu.co>
2024-10-01 15:59:16 -0500

    first commit

---
+++ src/test/java/com/lab2/Lab2/Lab2ApplicationTests.java
@@ -0,0 +1,68 @@
+package com.lab2.Lab2;
+
+import com.lab2.Lab2.controller.DataController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Autowired
+	DataController dataController;
+	@Test
+	void health() {
+		assertEquals("HEALTH CHECK OK!", dataController.healthCheck());
+	}
+	@Test
+	void version() {
+		assertEquals("The actual version is 1.0.0", dataController.version());
+	}
+	@Test
+	void nationLength() {
+		Integer nationsLength = dataController.getRandomNations().size();
+		assertEquals(10, nationsLength);
+	}
+	@Test
+	void currenciesLength() {
+		Integer currenciesLength =
+				dataController.getRandomCurrencies().size();
+		assertEquals(20, currenciesLength);
+	}
+	@Test
+	public void testRandomCurrenciesCodeFormat() {
+		DataController controller = new DataController();
+		JsonNode response = controller.getRandomCurrencies();
+		for (int i = 0; i < response.size(); i++) {
+			JsonNode currency = response.get(i);
+			String code = currency.get("code").asText();
+			assertTrue(code.matches("[A-Z]{3}")); // Check for 3 uppercase letters format
+		}
+	}
+	@Test
+	public void testRandomNationsPerformance() {
+		DataController controller = new DataController();
+		long startTime = System.currentTimeMillis();
+		controller.getRandomNations();
+		long endTime = System.currentTimeMillis();
+		long executionTime = endTime - startTime;
+		System.out.println(executionTime);
+// Assert that execution time is within acceptable limits
+		assertTrue(executionTime < 2000); // 2 second threshold
+	}
+	@Test
+	void aviationsLength() {
+		Integer aviationsLength = dataController.getRandomnAviation().size();
+		assertEquals(20, aviationsLength);
+	}
+
+
+}
