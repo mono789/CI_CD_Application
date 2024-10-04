c6f0d6b0a9b29eda0ffc43d34f04c72131af62e7
mono789 <juan.rivera7@udea.edu.co>
2024-10-04 10:36:33 -0500

    test

--- src/main/java/com/lab2/Lab2/controller/DataController.java
+++ src/main/java/com/lab2/Lab2/controller/DataController.java
@@ -11,6 +11,7 @@ import java.util.Locale;
 @RestController
 public class DataController {
     @GetMapping("/")
+    //hola
     public String healthCheck() {
         return "HEALTH CHECK OK!";
     }
